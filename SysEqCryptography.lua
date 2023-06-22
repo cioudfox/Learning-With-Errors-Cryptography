e32a11b7100783db363c73e0f717592dfa19c94c
Jason Wu <68248379+cioudfox@users.noreply.github.com>
2023-06-21 22:13:16 -0700

    Add files via upload

--- SysEqCryptography.lua
+++ SysEqCryptography.lua
@@ -1,27 +1,29 @@
 math.randomseed( os.time() )
 
 -- Private Key: Values for Variables(V,X,Y,Z)
-local privatekey = {10, 5, 15, 30}
+-- Any set of integer values
+local privatekey = {3, -9, 34, -47}
 
---[[ Changeable Values
+--[[ Changeable Values for Public Key 
      - sysEq: Table for system of equations
      - modVal: Prime number for Modular Calculations
      - Alphabet Size: Size of the Alphabet(A-Z = 26 characters)
      - Offset Value: Possible distance of error
        - Offset < [(alphsize) / (Variable number)]
-       - 5 < [(523 / 26) / 4 ]   =>   5 < 5.02
+       - 4 < [(523 / 26) / 4 ]   =>   4 < 5.02
 ]]
-
 local sysEq = {}
 local modVal = 523
 local alphsize = modVal // 26
-local offsetVal = 5
+local offsetVal = 4
 
 -- original msg -> HELLOWORLD
 -- msg2: returned equation after encoding
 local msg = {8,5,12,12,15,23,15,18,12,4}
 local msg2 = {}
 
+
+-- Generates Public key(List of linear equations)
 function generateLists()
   for i = 1,1000
   do
@@ -37,71 +39,72 @@ function generateLists()
 end
 
 
+-- Encodes the Message using random partition selections of Public Key
 function generateEncoded()
-  --Selection of 5 different unique indexes
   for i = 1, #msg
   do
-  local i1 = math.random(200)
-  local i2 = math.random(201,400)
-  local i3 = math.random(401,600)
-  local i4 = math.random(601,800)
-  local i5 = math.random(801,1000)
-  
-  local foo = {sysEq[i1][1] + sysEq[i2][1] + sysEq[i3][1] + sysEq[i4][1] + sysEq[i5][1],
-               sysEq[i1][2] + sysEq[i2][2] + sysEq[i3][2] + sysEq[i4][2] + sysEq[i5][2],
-               sysEq[i1][3] + sysEq[i2][3] + sysEq[i3][3] + sysEq[i4][3] + sysEq[i5][3],
-               sysEq[i1][4] + sysEq[i2][4] + sysEq[i3][4] + sysEq[i4][4] + sysEq[i5][4]
-  }
-
-  -- Sum of Summation Values
-  local tempval = ((sysEq[i1][5] + sysEq[i2][5] + sysEq[i3][5] + sysEq[i4][5] + sysEq[i5][5]) % modVal)
-  
-  --[[ Partition of Equations 
-       - Public Information:
-         - List of 1000 equations with Errors
-         - All results in the RHS of the "=" are Sums with offset modulus large prime
-       - Select a random partition of equations(5 in this example)
-       - Sum up the 5 equations to formulate a new combined Equation
-  ]]
-  
-  print("\nLinear Equation Selection for Character #" .. i)
-  print("Equation 1 : " .. sysEq[i1][1] .. "v + " .. sysEq[i1][2] .."x + " ..
-        sysEq[i1][3] .. "y + " .. sysEq[i1][4] .. "z = " .. (sysEq[i1][5] % modVal))
-  print("Equation 2 : " .. sysEq[i2][1] .. "v + " .. sysEq[i2][2] .."x + " ..
-        sysEq[i2][3] .. "y + " .. sysEq[i2][4] .. "z = " .. (sysEq[i2][5] % modVal))
-  print("Equation 3 : " .. sysEq[i3][1] .. "v + " .. sysEq[i3][2] .."x + " ..
-        sysEq[i3][3] .. "y + " .. sysEq[i3][4] .. "z = " .. (sysEq[i3][5] % modVal))
-  print("Equation 4 : " .. sysEq[i4][1] .. "v + " .. sysEq[i4][2] .."x + " ..
-        sysEq[i4][3] .. "y + " .. sysEq[i4][4] .. "z = " .. (sysEq[i4][5] % modVal))
-  print("Equation 5 : " .. sysEq[i5][1] .. "v + " .. sysEq[i5][2] .."x + " ..
-        sysEq[i5][3] .. "y + " .. sysEq[i5][4] .. "z = " .. (sysEq[i5][5] % modVal))
-  
-  print("\nEquation Combined : " .. foo[1] .. "v + " .. foo[2] .. "x + " .. foo[3] ..
-        "y + " .. foo[4] .. "z = " .. tempval)
-      
-  --[[ Encoding:
-       - Partition modulus value 257 into 26 parts
-       - For example, prime 257 =>  257 // 26 = 9
-       - Each alphabet letter can correspond to a number 1 - 26
-       - For example, adding a "c" can be done w/ 3 * 9 = 27 
-         added to the combined equation right side of the equal 
-  ]]
-  summfoo = ((msg[i] * alphsize) + tempval) % modVal
-  msg2[i] = {foo[1],foo[2],foo[3],foo[4],summfoo}
+    --Select 5 random linear equations to combine
+    local i1 = math.random(200)
+    local i2 = math.random(201,400)
+    local i3 = math.random(401,600)
+    local i4 = math.random(601,800)
+    local i5 = math.random(801,1000)
+    
+    --Calculations for combined equations
+    local foo = {sysEq[i1][1] + sysEq[i2][1] + sysEq[i3][1] + sysEq[i4][1] + sysEq[i5][1],
+                sysEq[i1][2] + sysEq[i2][2] + sysEq[i3][2] + sysEq[i4][2] + sysEq[i5][2],
+                sysEq[i1][3] + sysEq[i2][3] + sysEq[i3][3] + sysEq[i4][3] + sysEq[i5][3],
+                sysEq[i1][4] + sysEq[i2][4] + sysEq[i3][4] + sysEq[i4][4] + sysEq[i5][4]
+    }
+    local tempval = ((sysEq[i1][5] + sysEq[i2][5] + sysEq[i3][5] + sysEq[i4][5] + sysEq[i5][5]) % modVal)
+    
+    --[[ Partition of Equations 
+        - Public Information:
+          - List of 1000 equations with Errors
+          - All results in the RHS of the "=" are values modulus 257
+        - Bob selects a random partition of equations(5 in this example)
+        - Sum up the 5 equations to formulate a new Equation
+    ]]
+    print("\nIndex Selection for Character #" .. i)
+    print("Equation 1 : " .. sysEq[i1][1] .. "v + " .. sysEq[i1][2] .."x + " ..
+          sysEq[i1][3] .. "y + " .. sysEq[i1][4] .. "z = " .. (sysEq[i1][5] % modVal))
+    print("Equation 2 : " .. sysEq[i2][1] .. "v + " .. sysEq[i2][2] .."x + " ..
+          sysEq[i2][3] .. "y + " .. sysEq[i2][4] .. "z = " .. (sysEq[i2][5] % modVal))
+    print("Equation 3 : " .. sysEq[i3][1] .. "v + " .. sysEq[i3][2] .."x + " ..
+          sysEq[i3][3] .. "y + " .. sysEq[i3][4] .. "z = " .. (sysEq[i3][5] % modVal))
+    print("Equation 4 : " .. sysEq[i4][1] .. "v + " .. sysEq[i4][2] .."x + " ..
+          sysEq[i4][3] .. "y + " .. sysEq[i4][4] .. "z = " .. (sysEq[i4][5] % modVal))
+    print("Equation 5 : " .. sysEq[i5][1] .. "v + " .. sysEq[i5][2] .."x + " ..
+          sysEq[i5][3] .. "y + " .. sysEq[i5][4] .. "z = " .. (sysEq[i5][5] % modVal))
+    
+    print("\nEquation Combined : " .. foo[1] .. "v + " .. foo[2] .. "x + " .. foo[3] ..
+          "y + " .. foo[4] .. "z = " .. tempval)
+        
+    --[[ Encoding:
+        - Partition modulus value 257 into 26 parts
+        - 257 // 26 = 9
+        - Each alphabet letter can correspond to a number 1 - 26
+        - For example, adding a "c" can be done w/ 3 * 9 = 27 
+          added to the combined equation right side of the equal 
+    ]]
+    summfoo = ((msg[i] * alphsize) + tempval) % modVal
+    msg2[i] = {foo[1],foo[2],foo[3],foo[4],summfoo}
   end
 end
 
 
+-- Decode message using private key to remove offset and retreive msg values
 function decodeMessage()
   --[[ Decoding:
-       - Alice can calculate the actual values for the combined equations
+       - Private Key can be used to calculate actual expected values
        - Subtract Error Sums with Actual expected sums to get encErr
-       - encErr contains both the message plus the error bit(1-4)
+       - encErr contains both the message plus the error amount
+       - offset < distance so offset will be less than 1
        - Integer division of the encErr by 9 gets the actual value
+
   ]]
+  --Calculate list accurateValues using private key plugged into encoded equation
   local accValList = {}
-  
-  --Calculate accurate values using privatekey
   for i = 1, #msg2
   do
     local accVal = ((msg2[i][1] * privatekey[1]) + (msg2[i][2] * privatekey[2]) +
@@ -109,22 +112,22 @@ function decodeMessage()
     table.insert(accValList, accVal)
   end
   
-  --Calculate Message by Subtracting Error Summation by accurate values
+  --Calculate Message+Distance by Subtracting Error Summation by Actual Value
   print("")
   for i = 1, #msg2
   do
     local encErr = msg2[i][5] - accValList[i]
-    --print(encErr)
+
+    --If negative, find positive reciprocal in modulus
     if encErr < 0
     then
       encErr = encErr + modVal
     end
 
     print("Value #"..i)
-    print("Decoded Message w/ Error", encErr / alphsize)
-    print("Correct Decoded Value: ".. encErr // alphsize,"\n")
+    print("Encoded Message w/ Error", encErr / alphsize)
+    print("Decoded Value: ".. encErr // alphsize,"\n")
   end
-  
 end
 
 generateLists()
